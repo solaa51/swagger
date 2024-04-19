d8c44987e82c9f3e8ef4b972d7975eaedb162c53
李苏亮 <460304623@qq.com>
2024-04-19 11:34:23 +0800

    整理目录

--- example/main.go
+++ example/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func init() {
-	//加载路由配置
+	//配置路由规则
 	r := &router.RouteParse{}
 	r.Prefix("api").BindFunc("welcome/index2", func(ctx *context.Context) {
 		ctx.RetData = "hello world2"
@@ -20,24 +20,6 @@ func init() {
 }
 
 func main() {
-	fmt.Println("kaishi")
-
 	appServer.Run()
-
 	// 中间件 或 路由 是否有必要支持  局部方法 限流处理
-}
-
-func hand() {
-	//handleFuncParse.ParseFunc("welcome/index", func(context *context.Context) {
-	//	fmt.Println("hello world")
-	//})
-
-	//h := handler.MatchAndDelHandler("welcome/index")
-	//fmt.Println(h.Call(nil))
-
-	//h := handler.ClearHandlerToRoute()
-	//fmt.Println(h["welcome/index"].Call(nil))
-
-	//h := handler.MatchPrefixAndDelHandler("welcome")
-	//fmt.Println(h)
 }
